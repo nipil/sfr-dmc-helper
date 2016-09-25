810b8654460b0e71697c1bf62ce14f2589419b3f
nipil <nipil@users.noreply.github.com>
2016-09-25 14:24:56 +0200

    select spaces

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys, ConfigParser
+import sys, ConfigParser, requests, json, time, logging
 
 class Parameters:
 
@@ -10,6 +10,7 @@ class Parameters:
         self.auth_config_file = auth_config_file
         self.auth_service_id = None
         self.auth_service_password = None
+        self.auth_space = None
 
     def load_config(self, file):
         self.current_config_file = file
@@ -26,51 +27,123 @@ class Parameters:
         try:
             return config.get(section, param)
         except (ConfigParser.NoSectionError, ConfigParser.NoOptionError) as e:
-            print "Erreur de configuration dans le fichier " + self.current_config_file + " : " + e.message
-            raise
+            print "Erreur de configuration dans le fichier %s : %s" % (self.current_config_file, e.message)
             sys.exit(1)
 
     def load_configs(self):
         self.parse_auth(self.auth_config_file)
 
+class Api:
 
-class SpaceMenu:
+    BASE_URL = "https://www.dmc.sfr-sh.fr/DmcWS/1.5.1/JsonService"
 
     def __init__(self, parameters):
         self.parameters = parameters
 
+    def create_authenticate_params(self, cur_params, space = False):
+        cur_params["authenticate"] = {
+            "serviceId": self.parameters.auth_service_id,
+            "servicePassword": self.parameters.auth_service_password,
+            "lang": "fr_FR"
+        }
+        if space:
+            if self.parameters.auth_space is None:
+                raise Exception("Aucun espace sélectionné, veuillez sélectionner un espace d'abord")
+            cur_params["authenticate"]["spaceId"] = self.parameters.auth_space
+        cur_params["authenticate"] = json.dumps(cur_params["authenticate"])
+
+    def post(self, url, content):
+        t0 = time.time()
+        r = requests.post(url, data = content)
+        t1 = time.time()
+        print "Requête exécutée en %f secondes" % (t1-t0)
+        try:
+            j = json.loads(r.text)
+            if not j["success"]:
+                raise Exception("Erreur lors de la requête. Données retournées : %s" % j)
+            return j
+        except Exception, e:
+            print e
+            raise
+
+    def findSpaces(self):
+        p = {}
+        self.create_authenticate_params(p)
+        return self.post("%s/AdminWS/findSpaces" % Api.BASE_URL, p)
+
+class Menu:
+
+    def __init__(self, invite):
+        self.menu_invite = invite
+
     def get_invite(self):
-        return "Sélection des espaces"
+        return self.menu_invite
 
-    def run(self):
-        print "space menu"
-        pass
+    def interact(self, content):
+        s = None
+        while True:
+            print self.get_invite()
+            for k, v in content.iteritems():
+                print k, ".", v.get_invite()
+            print "x", ".", "Retour"
+            s = raw_input(">> ")
+            if s == 'x':
+                return
+            elif s not in content.keys():
+                continue
+            else:
+                content[s].run()
 
-class MainMenu:
+    def interactValue(self, content):
+        s = None
+        while True:
+            print self.get_invite()
+            for k, v in content.iteritems():
+                print k, ".", v
+            print "x", ".", "Retour"
+            s = raw_input(">> ")
+            print { "aze" : s }
+            if s == 'x':
+                return None
+            elif s not in content.keys():
+                continue
+            else:
+                return s
+
+class SpaceMenu(Menu):
 
     def __init__(self, parameters):
+        Menu.__init__(self, "Sélection des espaces")
+        self.parameters = parameters
+        self.spaces = None
+
+    def get_space(self):
+        s = Api(self.parameters).findSpaces()
+        self.spaces = {}
+        for v in s["response"]["list"]:
+            self.spaces[str(v["spaceId"])] = "%s (%s)" % (
+                v["spaceName"],
+                "actif" if v["spaceActive"] else "inactif"
+            )
+
+    def run(self):
+        if self.spaces is None:
+            self.get_space()
+        r = Menu.interactValue(self, self.spaces)
+        self.parameters.auth_space = int(r)
+        print "Espace sélectionné : %i" % self.parameters.auth_space
+
+class MainMenu(Menu):
+
+    def __init__(self, parameters):
+        Menu.__init__(self, "Menu principal")
         self.parameters = parameters
         self.menus = {
             "1": SpaceMenu(self.parameters),
         }
 
-    def get_invite(self):
-        return "Menu principal"
-
     def run(self):
-        s = None
-        while True:
-            print self.get_invite()
-            for k, v in self.menus.iteritems():
-                print k + ".", v.get_invite()
-            print "x. Retour"
-            s = raw_input(" >> ")
-            if s == 'x':
-                return
-            elif s not in self.menus.keys():
-                continue
-            else:
-                self.menus[s].run()
+        Menu.interact(self, self.menus)
 
 class App:
 
@@ -81,7 +154,22 @@ class App:
         self.parameters.load_configs()
         self.main_menu = MainMenu(self.parameters)
 
+    def setup_logging(self):
+        logging.basicConfig()
+        if len(sys.argv) != 2 or sys.argv[1] != "--debug":
+            return
+        try:
+            import http.client as http_client
+        except ImportError:
+            import httplib as http_client
+            logging.getLogger().setLevel(logging.DEBUG)
+        http_client.HTTPConnection.debuglevel = 1
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
+
     def run(self):
+        self.setup_logging()
         self.main_menu.run()
 
 if __name__ == "__main__":
